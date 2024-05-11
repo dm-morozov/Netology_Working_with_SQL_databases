a7a61781696172d820f9d3977c788b957a585738
Dmitriy Morozov (PC) <dem.morozov@gmail.com>
2024-05-11 14:35:06 +0500

    Add homework solutions

--- creating_a_database_music_service.sql
+++ creating_a_database_music_service.sql
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS Artist_Album (
 );
 
 CREATE TABLE IF NOT EXISTS Track (
-    if SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(40) NOT NULL,
     duration INTEGER NOT NULL,
     album_id INTEGER NOT NULL REFERENCES Album(id)
@@ -46,3 +46,81 @@ CREATE TABLE IF NOT EXISTS Collection_Track (
     collection_id INTEGER NOT NULL REFERENCES Collection(id),
     track_id INTEGER NOT NULL REFERENCES Track(id)
 );
+
+-- alter table track rename if to id;
+
+insert into genre(name) values 
+('Pop'), 
+('Rock'), 
+('Hip-Hop');
+insert into genre(name) values ('Rap');
+select * from genre;
+
+insert into artist(name) values 
+('Егор Крид'), 
+('Валя Карнавал'), 
+('Коля Басков'), 
+('Моргенштерн');
+select * from artist;
+
+insert into album(name, year) values 
+('PUSSY BOY', 2023), 
+('Маме верь', 2021), 
+('Обниму тебя', 2020), 
+('Лез Гризи', 2022);
+select * from album;
+
+insert into track (name, duration, album_id) values
+('Pussy Boy', 180, 1),
+('Потрачу', 195, 1),
+('Ты меня не была', 210, 2),
+('Жути', 165, 2),
+('Обниму тебя', 220, 3),
+('Любовью за периметром', 240, 3),
+('Я ж гризи', 140, 4),
+('Оксимирон убил Мирона', 170, 4);
+select * from track;
+
+
+select * from collection;
+insert into collection (name, year) values 
+('Бодрое утро', 2023),
+('Поп-новинки', 2023),
+('Женский вокал', 2021),
+('Популярная эстрада', 2023),
+('Новая школа рэпа', 2023);
+
+select * from genres_artists;
+insert into genres_artists (gender_id, artists_id) values
+(1, 1),
+(2, 4),
+(3, 1),
+(1, 4),
+(4, 1),
+(4, 4);
+
+select * from artist_album;
+insert into artist_album(artist_id, album_id) values
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4);
+
+select * from collection_track;
+insert into collection_track(collection_id, track_id) values
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 7),
+(1, 8),
+(2, 1),
+(2, 2),
+(3, 3),
+(3, 4),
+(4, 5),
+(4, 6),
+(5, 1),
+(5, 2),
+(5, 7),
+(5, 8);
